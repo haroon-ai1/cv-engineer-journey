ed51875d6fbd1505a177eeb9eca7422dc585e2fb
haroon-ai1 <muhammadharoon8124@gmail.com>
2026-05-04 02:10:33 +0500

    Day 1 -tensor basics

---
+++ First.py
@@ -0,0 +1,11 @@
+#first code
+import torch
+
+x= torch.tensor([1.0,2.0,3.0])
+print("shape :",x.shape)
+print("dtype :",x.dtype)
+
+print("add:", x + x)
+print('multily :',x*2)
+print("squre :",x**2)
+
